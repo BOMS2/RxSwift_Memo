cc6977a3b6473db5dfe6b48aa54059acc4e0e7a2
bomin <kbm97121@gmail.com>
2020-04-08 02:43:26 +0900

    memodetailviewmodel 구현

--- RxMemo_BM/ViewModel/MemoDetailViewModel.swift
+++ RxMemo_BM/ViewModel/MemoDetailViewModel.swift
@@ -7,7 +7,31 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
     
+    //이전 scene에서 저장된 메모
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "Ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        return f
+    }()
+    
+    var contents: BehaviorSubject<[String]>.Observer
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType){
+        self.memo = memo
+        
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content, formatter.string(from: memo.insertData)
+        ])
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
