4b01f2932cad12c74e838aa02ad3cea21a33747b
bomin <kbm97121@gmail.com>
2020-04-07 20:23:07 +0900

    memory storage 추가

---
+++ RxMemo_BM/Service/MemoStorageType.swift
@@ -0,0 +1,25 @@
+//
+//  MemoStorageType.swift
+//  RxMemo_BM
+//
+//  Created by 김삼복 on 07/04/2020.
+//  Copyright © 2020 김삼복. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+//기본적인 CRUD를 처리
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
---
+++ RxMemo_BM/Service/MemoryStorage/MemoryStorage.swift
@@ -0,0 +1,9 @@
+//
+//  MemoryStorage.swift
+//  RxMemo_BM
+//
+//  Created by 김삼복 on 07/04/2020.
+//  Copyright © 2020 김삼복. All rights reserved.
+//
+
+import Foundation
