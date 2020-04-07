d8e8f4f6c9af7d3ab60678533e1f0fbefcbc51df
bomin <kbm97121@gmail.com>
2020-04-07 20:39:22 +0900

    view controller  class 추가

---
+++ RxMemo_BM/ViewController/MemoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoDetailViewController.swift
+//  RxMemo_BM
+//
+//  Created by 김삼복 on 07/04/2020.
+//  Copyright © 2020 김삼복. All rights reserved.
+//
+
+import UIKit
+
+class MemoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
