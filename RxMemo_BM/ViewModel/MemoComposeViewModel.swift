feb8cfb7871192c8c01d6990fa722763bba1f143
bomin <kbm97121@gmail.com>
2020-04-08 02:04:36 +0900

    memocomposeviewmodel 추가
    새로운 메모 추가하고 편집하는 기능

--- RxMemo_BM/ViewModel/MemoComposeViewModel.swift
+++ RxMemo_BM/ViewModel/MemoComposeViewModel.swift
@@ -7,7 +7,43 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+import Action
 
-class MemoComposeViewModel {
+//새로운 메모를 추가하고 편집할 때 사용
+class MemoComposeViewModel: CommonViewModel {
+    private let content: String?
     
+    var initialText: Driver<String?> {
+        return Observable.just(content).asDriver(onErrorJustReturn: nil)
+    }
+    
+    let saveAction: Action<String, Void>
+    let cancelAction: CocoaAction
+    
+    init(title: String, content: String? = nil, sceneCoordinator:
+        SceneCoordinatorType, storage: MemoStorageType, saveAction: Action<String, Void>? = nil, cancelAction: CocoaAction? = nil) {
+        self.content = content
+        
+        //여기서 action으로 한 번 더 래핑함
+        self.saveAction = Action<String, Void> { input in
+            if let action = saveAction {
+                action.execute(input)
+            }
+            
+            //액션 전달 안되면 화면 닫고 끝남
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        self.cancelAction = CocoaAction {
+            if let action = cancelAction {
+                action.execute(())
+            }
+            
+            return sceneCoordinator.close(animated: true).asObservable().map { _ in }
+        }
+        
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
 }
